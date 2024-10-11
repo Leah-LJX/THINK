b504036d4c96802cc086fb6017649cf909eafbe7
Jiaxin Liu <34062191+Leah-LJX@users.noreply.github.com>
2024-10-11 09:52:01 +0800

    Add files via upload

---
+++ Error.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Error(Enum):
+    IMPORT_ERROR = 1
+    PARA_FILL_ERROR = 2
+    METHOD_CALL_ERROR = 3
+    TYPE_MISMATCH_ERROR = 4  # 未实现处理
+    EXCEPTION_ERROR = 5
+    NONSTATIC_ERROR = 6
+    VARDEF_ERROR = 7
